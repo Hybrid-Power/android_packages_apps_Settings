fbcd243f6b9bc68ea96d84a1abed1dc72a672e83
apr20th311 <apr20th311@gmail.com>
2015-08-04 12:44:13 +0800

    Revert "carrier"
    This reverts commit 174944917c54716478635907863a4d36da4c0311.

--- src/com/android/settings/cyanogenmod/StatusBarSettings.java
+++ src/com/android/settings/cyanogenmod/StatusBarSettings.java
@@ -15,12 +15,7 @@
  */
 package com.android.settings.cyanogenmod;
 
-
-import android.app.AlertDialog;
 import android.content.ContentResolver;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.database.ContentObserver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
@@ -29,16 +24,9 @@ import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
-import android.preference.PreferenceScreen;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
-import android.preference.SwitchPreference;
 import android.telephony.TelephonyManager;
-import android.text.Spannable;
-import android.text.TextUtils;
-import android.text.format.DateFormat;
-import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.text.format.DateFormat;
 import android.view.View;
 
@@ -61,9 +49,6 @@ public class StatusBarSettings extends SettingsPreferenceFragment
     private static final String STATUS_BAR_CLOCK_STYLE = "status_bar_clock";
     private static final String STATUS_BAR_AM_PM = "status_bar_am_pm";
     private static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
-    private static final String STATUS_BAR_CARRIER = "status_bar_carrier";
-    private static final String CUSTOM_CARRIER_LABEL = "custom_carrier_label";
-    private static final String CARRIER_SIZE_STYLE = "carrier_size_style";
     private static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
     private static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
     private static final String NETWORK_TRAFFIC_UNIT = "network_traffic_unit";
@@ -89,41 +74,14 @@ public class StatusBarSettings extends SettingsPreferenceFragment
     private SystemSettingSwitchPreference mNetTrafficAutohide;
     private SeekBarPreference mNetTrafficAutohideThreshold;
 
-    private SwitchPreference mStatusBarCarrier;
-    private PreferenceScreen mCustomCarrierLabel;
-    private ListPreference mCarrierSize;
-    private String mCustomCarrierLabelText;
-
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.status_bar_settings);
 
-        PreferenceScreen prefSet = getPreferenceScreen();
         ContentResolver resolver = getActivity().getContentResolver();
 
         loadResources();
-
-        mCarrierSize = (ListPreference) findPreference(CARRIER_SIZE_STYLE);
-        int CarrierSize = Settings.System.getInt(resolver,
-                Settings.System.CARRIER_SIZE, 5);
-        mCarrierSize.setValue(String.valueOf(CarrierSize));
-        mCarrierSize.setSummary(mCarrierSize.getEntry());
-        mCarrierSize.setOnPreferenceChangeListener(this);
-
-        mStatusBarCarrier = (SwitchPreference) prefSet
-                .findPreference(STATUS_BAR_CARRIER);
-        mStatusBarCarrier.setChecked((Settings.System.getInt(resolver,
-                Settings.System.STATUS_BAR_CARRIER, 0) == 1));
-        mStatusBarCarrier.setOnPreferenceChangeListener(this);
-        mCustomCarrierLabel = (PreferenceScreen) prefSet
-                .findPreference(CUSTOM_CARRIER_LABEL);
-        if (TelephonyManager.getDefault().isMultiSimEnabled()) {
-            prefSet.removePreference(mStatusBarCarrier);
-            prefSet.removePreference(mCustomCarrierLabel);
-        } else {
-            updateCustomLabelTextSummary();
-        }
 
         mStatusBarClock = (ListPreference) findPreference(STATUS_BAR_CLOCK_STYLE);
         mStatusBarAmPm = (ListPreference) findPreference(STATUS_BAR_AM_PM);
@@ -190,18 +148,6 @@ public class StatusBarSettings extends SettingsPreferenceFragment
                 Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD, 10);
         mNetTrafficAutohideThreshold.setValue(netTrafficAutohideThreshold / 1);
         mNetTrafficAutohideThreshold.setOnPreferenceChangeListener(this);
-    }
-
-    private void updateCustomLabelTextSummary() {
-        mCustomCarrierLabelText = Settings.System.getString(getActivity()
-                .getContentResolver(), Settings.System.CUSTOM_CARRIER_LABEL);
-
-        if (TextUtils.isEmpty(mCustomCarrierLabelText)) {
-            mCustomCarrierLabel
-                    .setSummary(R.string.custom_carrier_label_notset);
-        } else {
-            mCustomCarrierLabel.setSummary(mCustomCarrierLabelText);
-        }
     }
 
     @Override
@@ -279,71 +225,8 @@ public class StatusBarSettings extends SettingsPreferenceFragment
             Settings.System.putInt(resolver,
                     Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD, threshold * 1);
             return true;
-        } else if (preference == mStatusBarCarrier) {
-            boolean value = (Boolean) newValue;
-            Settings.System.putInt(resolver,
-                    Settings.System.STATUS_BAR_CARRIER, value ? 1 : 0);
-            Intent i = new Intent();
-            i.setAction(Intent.ACTION_CUSTOM_CARRIER_LABEL_CHANGED);
-            getActivity().sendBroadcast(i);
-            return true;
-        } else if (preference == mCarrierSize) {
-            int CarrierSize = Integer.valueOf((String) newValue);
-            int index = mCarrierSize.findIndexOfValue((String) newValue);
-            Settings.System.putInt(resolver, Settings.System.CARRIER_SIZE,
-                    CarrierSize);
-            mCarrierSize.setSummary(mCarrierSize.getEntries()[index]);
-            Intent i = new Intent();
-            i.setAction(Intent.ACTION_CUSTOM_CARRIER_LABEL_CHANGED);
-            getActivity().sendBroadcast(i);
-            return true;
         }
         return false;
-    }
-
-    @Override
-    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
-            final Preference preference) {
-        final ContentResolver resolver = getActivity().getContentResolver();
-        if (preference.getKey().equals(CUSTOM_CARRIER_LABEL)) {
-            AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
-            alert.setTitle(R.string.custom_carrier_label_title);
-            alert.setMessage(R.string.custom_carrier_label_explain);
-            LinearLayout parent = new LinearLayout(getActivity());
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT,
-                    LinearLayout.LayoutParams.MATCH_PARENT);
-            parent.setLayoutParams(params);
-            // Set an EditText view to get user input
-            final EditText input = new EditText(getActivity());
-            input.setText(TextUtils.isEmpty(mCustomCarrierLabelText) ? ""
-                    : mCustomCarrierLabelText);
-            input.setSelection(input.getText().length());
-            params.setMargins(60, 0, 60, 0);
-            input.setLayoutParams(params);
-            parent.addView(input);
-            alert.setView(parent);
-            alert.setPositiveButton(getString(android.R.string.ok),
-                    new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog,
-                                int whichButton) {
-                            String value = ((Spannable) input.getText())
-                                    .toString().trim();
-                            Settings.System
-                                    .putString(
-                                            resolver,
-                                            Settings.System.CUSTOM_CARRIER_LABEL,
-                                            value);
-                            updateCustomLabelTextSummary();
-                            Intent i = new Intent();
-                            i.setAction(Intent.ACTION_CUSTOM_CARRIER_LABEL_CHANGED);
-                            getActivity().sendBroadcast(i);
-                        }
-                    });
-            alert.setNegativeButton(getString(android.R.string.cancel), null);
-            alert.show();
-        } 
-        return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 
     private void enableStatusBarBatteryDependents(int batteryIconStyle) {
